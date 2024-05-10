9c11649e42c7bfdb8d191c52ff3054b98217f83f
SilhouettesForYou <mgze.wang@gmail.com>
2024-05-10 23:00:33 +0800

    Site updated: 2024-05-10 22:59:26

---
+++ js/rose.js
@@ -0,0 +1,72 @@
+var ROOT_PATH = 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples';
+
+var sin = Math.sin;
+var cos = Math.cos;
+var pow = Math.pow;
+var sqrt = Math.sqrt;
+var cosh = Math.cosh;
+var sinh = Math.sinh;
+var exp = Math.exp;
+var PI = Math.PI;
+var square = function (x) {
+  return x * x;
+};
+var mod2 = function (a, b) {
+  var c = a % b;
+  return c > 0 ? c : c + b;
+};
+var theta1 = -(20 / 9) * PI;
+var theta2 = 15 * PI;
+function getParametricEquation() {
+  return {
+    u: {
+      min: 0,
+      max: 1,
+      step: 1 / 24
+    },
+    v: {
+      min: theta1,
+      max: theta2,
+      step: (theta2 - theta1) / 575
+    },
+    x: function (x1, theta) {
+      var phi = (PI / 2) * exp(-theta / (8 * PI));
+      var y1 =
+        1.9565284531299512 *
+        square(x1) *
+        square(1.2768869870150188 * x1 - 1) *
+        sin(phi);
+      var X =
+        1 -
+        square(1.25 * square(1 - mod2(3.6 * theta, 2 * PI) / PI) - 0.25) / 2;
+      var r = X * (x1 * sin(phi) + y1 * cos(phi));
+      return r * sin(theta);
+    },
+    y: function (x1, theta) {
+      var phi = (PI / 2) * exp(-theta / (8 * PI));
+      var y1 =
+        1.9565284531299512 *
+        square(x1) *
+        square(1.2768869870150188 * x1 - 1) *
+        sin(phi);
+      var X =
+        1 -
+        square(1.25 * square(1 - mod2(3.6 * theta, 2 * PI) / PI) - 0.25) / 2;
+      var r = X * (x1 * sin(phi) + y1 * cos(phi));
+      return r * cos(theta);
+    },
+    z: function (x1, theta) {
+      var phi = (PI / 2) * exp(-theta / (8 * PI));
+      var y1 =
+        1.9565284531299512 *
+        square(x1) *
+        square(1.2768869870150188 * x1 - 1) *
+        sin(phi);
+      var X =
+        1 -
+        square(1.25 * square(1 - mod2(3.6 * theta, 2 * PI) / PI) - 0.25) / 2;
+      var r = X * (x1 * sin(phi) + y1 * cos(phi));
+      return X * (x1 * cos(phi) - y1 * sin(phi));
+    }
+  };
+}
